e326edda2e50bf0ae13095116f852df07bef6aa0
Konstantin <kostia.talamaniuk@gmail.com>
2022-09-06 18:34:41 +0300

    merge++

--- js/script20.js
+++ js/script20.js
@@ -1,1 +1,35 @@
 'use strict';
+
+// if (1 + 1 == 3) {
+//     console.log('OK!');
+// } else {
+//     console.log('Error');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('OK!');
+// }
+
+// (num === 50) ? console.log('Ok!') : console.log('Error');
+
+const num = 10;
+switch (num) {
+    case 49: 
+        console.log('Error');
+        break;
+    case 100:
+        console.log('Error');
+        break;
+    case 10:
+        console.log('Bingo!');
+        break;
+    default:
+        console.log('Better luck next time');
+        break;
+}
